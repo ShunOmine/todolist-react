3bd7db1575bd8e747032dd7b085d46c25ee3c2fe
ShunOmine <shun12baseball@gmail.com>
2019-04-04 19:19:27 +0900

    create todo todostore todoactions

---
+++ src/stores/TodoStore.js
@@ -0,0 +1,45 @@
+import { EventEmitter } from "events";
+
+import dispatcher from "../dispatcher";
+
+class TodoStore extends EventEmitter{
+    constructor() {
+        super();
+        this.todos = [
+            {
+                id: 11346413,
+                text: "Go Shopping",
+                complete: false
+            },
+            {
+                id: 23568479,
+                text: "Pay Water Bills",
+                complete: false
+            },
+        ]
+    };
+    createTodo(text){
+        const id = Date.now();
+        this.todos.push({
+            id,
+            text,
+            complete: false
+        });
+        this.emit("change");
+    }
+    getAll(){
+        return this.todos;
+    }
+    handleActions(action){
+        switch(action.type) {
+            case "CREATE_TODO": {
+                this.createTodo(action.text);
+            }
+        }
+    }
+}
+
+const todosStore = new TodoStore();
+dispatcher.register(todosStore.handleActions.bind(todosStore));
+window.dispatcher = dispatcher;
+export default todosStore;
