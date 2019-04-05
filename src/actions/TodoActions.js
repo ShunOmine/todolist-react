a7c8c723ffd8066e4a47d391614a88ddb3f43703
ShunOmine <shun12baseball@gmail.com>
2019-04-05 14:05:57 +0900

    create todostore reloadTodos and edit Todos

--- src/actions/TodoActions.js
+++ src/actions/TodoActions.js
@@ -13,3 +13,24 @@ export function deleteTodo(id) {
         id
     });
 }
+
+export function reloadTodos(){
+// axios("htttp://someurl.com/somedataendpoint").then((data) => {
+//      console.log("got the data!", data);
+// });
+    dispatcher.dispatch({type: "FETCH_TODOS"});
+    setTimeout(() => {
+        dispatcher.dispatch({type: "RECEIVE_TODOS", todos: [
+                {
+                    id: 21346413,
+                    text: "Go Shopping Again!",
+                    complete: false
+                },
+                {
+                    id: 335684679,
+                    text: "Sleep At the Yard.",
+                    complete: true
+                }
+        ]});
+    }, 1000);
+}
